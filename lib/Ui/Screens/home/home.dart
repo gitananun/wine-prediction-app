810741cd3799cd81a82402cd13e4737462a573f5
Tigran <Tigran@Lionixs-MacBook-Pro.local>
2021-05-13 18:04:43 +0400

    feedback haptic

--- lib/Ui/Screens/home/home.dart
+++ lib/Ui/Screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:schema/Domain/Actions/WinePredictAction.dart';
 
 import 'package:schema/Infrastructure/Validators/EmptyValidator.dart';
@@ -36,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   void _onPressed() async {
+    HapticFeedback.mediumImpact();
     setState(() => isLoading = true);
     if (_formKey.currentState!.validate()) {
       _controllers.forEach((label, controller) {
